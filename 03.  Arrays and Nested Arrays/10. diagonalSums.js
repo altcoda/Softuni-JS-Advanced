1ed7417383dbfb7ea7569a3304c96dddb3953b79
NadezhdaGeorgieva <nadezhdageorgieva@outlook.com>
2021-09-19 11:54:08 +0300

    Minor changes

--- 03.  Arrays and Nested Arrays/10. diagonalSums.js
+++ 03.  Arrays and Nested Arrays/10. diagonalSums.js
@@ -1,13 +1,12 @@
-function diagonalAttack(matrix) {
+function diagonalSums(matrix) {
 
-    let firstDiagonalSum = 0;
-    let secondDiagonalSum = 0;
+    let firstDiagonalSum = 0, secondDiagonalSum = 0;
     let v = matrix.length - 1;
     for (let i = 0; i < matrix.length; i++) {
       firstDiagonalSum += matrix[i][i];
       secondDiagonalSum += matrix[i][v];
       v--;
     }
-      
-    return `${firstDiagonalSum} ${secondDiagonalSum}`
+  
+    return `${firstDiagonalSum} ${secondDiagonalSum}` 
 }
