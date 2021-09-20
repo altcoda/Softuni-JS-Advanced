c713e4b9771e95965d6010fb146c51129282de2f
NadezhdaGeorgieva <nadezhdageorgieva@outlook.com>
2021-09-20 21:40:45 +0300

    Updated exercise

--- 05. Objects and Composition/03. cityTaxes.js
+++ 05. Objects and Composition/03. cityTaxes.js
@@ -1,19 +1,19 @@
 function cityTaxes(name, population, treasury) {
-    return {
-      name,
-      population,
-      treasury,
-      taxRate: 10,
-      collectTaxes: function() {
-        this.treasury += Math.floor(this.population * this.taxRate);
-      },
-      applyGrowth: function(pct) {
-        const increment = (pct / 100) * this.population;
-        this.population = Math.floor(this.population + increment);
-      },
-      applyRecession: function(pct) {
-        const decrement = (pct / 100) * this.treasury;
-        this.treasury = Math.floor(this.treasury - decrement);
-      }
+  return {
+    name,
+    population,
+    treasury,
+    taxRate: 10,
+    collectTaxes() {
+      this.treasury += Math.floor(this.population * this.taxRate);
+    },
+    applyGrowth(pct) {
+      const increment = (pct / 100) * this.population;
+      this.population = Math.floor(this.population + increment);
+    },
+    applyRecession(pct) {
+      const decrement = (pct / 100) * this.treasury;
+      this.treasury = Math.floor(this.treasury - decrement);
     }
+  }
 }
