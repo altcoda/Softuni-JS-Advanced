b7c38d64a80773e8db2fa1835e0efaa94c972e05
NadezhdaGeorgieva <nadezhdageorgieva@outlook.com>
2021-09-22 11:43:16 +0300

    Added comments and changed variable name

--- 04. Arrays and Nested Arrays - Exercise/11. diagonalAttack.js
+++ 04. Arrays and Nested Arrays - Exercise/11. diagonalAttack.js
@@ -1,27 +1,32 @@
 function diagonalAttack(matrix) {
 
+  // Convert matrix elements to numbers
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i] = matrix[i].split(' ').map(Number);
+  }
+
+  // Sum two main diagonals of matrix
+  let firstDiagonalSum = 0, secondDiagonalSum = 0;
+  let secondDiagonalPos = matrix.length - 1;
+  for (let i = 0; i < matrix.length; i++) {
+    firstDiagonalSum += matrix[i][i];
+    secondDiagonalSum += matrix[i][secondDiagonalPos];
+    secondDiagonalPos--;
+  }
+
+  // If sums are equal change all other numbers in matrix to first sum
+  if (firstDiagonalSum == secondDiagonalSum) {
+    secondDiagonalPos = matrix.length - 1;
     for (let i = 0; i < matrix.length; i++) {
-      matrix[i] = matrix[i].split(' ').map(Number)
+      matrix[i].map((num, index) => {
+        // If the numbers aren't part of the diagonals change them
+        if (index !== i && index !== secondDiagonalPos) {
+          matrix[i][index] = firstDiagonalSum;
+        }
+      })
+      secondDiagonalPos--;
     }
-  
-    let firstDiagonalSum = 0, secondDiagonalSum = 0;
-  
-    let v = matrix.length - 1;
-    for (let i = 0; i < matrix.length; i++) {
-      firstDiagonalSum += matrix[i][i];
-      secondDiagonalSum += matrix[i][v];
-      v--;
-    }
-  
-    if (firstDiagonalSum == secondDiagonalSum) {
-      v = matrix.length - 1;
-      for (let i = 0; i < matrix.length; i++) {
-        matrix[i].map((num, index) => {
-          if (index !== i && index !== v) { matrix[i][index] = firstDiagonalSum }
-        })
-        v--;
-      }
-    }
-  
-    matrix.forEach(line => console.log(line.join(' ')));
+  }
+
+  matrix.forEach(line => console.log(line.join(' ')));
 }
