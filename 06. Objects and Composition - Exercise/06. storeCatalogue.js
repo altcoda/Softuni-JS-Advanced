856e8cd6a7c3eb80a87ed2ae2ec7e3d2e5e7ced4
NadezhdaGeorgieva <nadezhdageorgieva@outlook.com>
2021-09-22 21:07:29 +0300

    Added Comments

--- 06. Objects and Composition - Exercise/06. storeCatalogue.js
+++ 06. Objects and Composition - Exercise/06. storeCatalogue.js
@@ -1,32 +1,35 @@
 function storeCatalogue(products) {
 
-    let catalogue = {}
-  
-    products.forEach(info => {
-      const [name, price] = info.split(' : ');
-      const letter = name.charAt(0);
-      if (!catalogue.hasOwnProperty(letter)) {
-        catalogue[letter] = {};
-        catalogue[letter][name] = Number(price);
-      } else {
-        catalogue[letter][name] = Number(price);
-      }
-    })
-  
-    const arr = [];
-    arr.push(catalogue)
-  
-    const sorted = Object.entries(catalogue).sort();
-  
-    for (let key in sorted) {
-      const entries = Object.entries(sorted[key][1]);
-      const sortedEntries = Array.from(entries).sort();
-  
-      const letter = sorted[key][0];
-      console.log(letter)
-      sortedEntries.map(entry => {
-        const [name, price] = [...entry];
-        console.log(`  ${name}: ${price}`)
-      })
+  let catalogue = {}
+
+  // Create a catalogue of products and prices
+  products.forEach(info => {
+    const [name, price] = info.split(' : ');
+    const letter = name.charAt(0);
+    if (!catalogue.hasOwnProperty(letter)) {
+      catalogue[letter] = {};
+      catalogue[letter][name] = Number(price);
+    } else {
+      catalogue[letter][name] = Number(price);
     }
+  })
+
+  const arr = [];
+  arr.push(catalogue)
+
+  // Sort outer layer alphabetically
+  const sorted = Object.entries(catalogue).sort();
+
+  for (let key in sorted) {
+    const letter = sorted[key][0];
+    console.log(letter)
+
+    // Sort product names alphabetically and print by letter
+    const entries = Object.entries(sorted[key][1]);
+    const sortedEntries = Array.from(entries).sort();
+    sortedEntries.map(entry => {
+      const [name, price] = [...entry];
+      console.log(`  ${name}: ${price}`)
+    })
+  }
 }
